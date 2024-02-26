9a0447f4273e72a41c8f8876c47ccd0cacab3431
Muunatic <ichigoez0@gmail.com>
2024-02-26 11:30:58 +0700

    test: add getForecastData() test

--- __tests__/index.test.cjs
+++ __tests__/index.test.cjs
@@ -1,5 +1,5 @@
-const weatherjs = require('../index.cjs');
-const weather = new weatherjs('en', 'c');
+const msnweather = require('../index.cjs');
+const weather = new msnweather('en', 'c');
 
 test('Get getCurrentData()', async () => {
     expect((await weather.getCurrentData('kyoto')).temperature).toBeDefined();
@@ -7,4 +7,13 @@ test('Get getCurrentData()', async () => {
     expect((await weather.getCurrentData('kyoto')).humidity).toBeDefined();
     expect((await weather.getCurrentData('kyoto')).windspeed).toBeDefined();
     expect((await weather.getCurrentData('kyoto')).location).toBeDefined();
+});
+
+test('Get getForecastData()', async () => {
+    expect((await weather.getForecastData('kyoto', 1)).lowTemperature).toBeDefined();
+    expect((await weather.getForecastData('kyoto', 1)).highTemperature).toBeDefined();
+    expect((await weather.getForecastData('kyoto', 1)).date).toBeDefined();
+    expect((await weather.getForecastData('kyoto', 1)).day).toBeDefined();
+    expect((await weather.getForecastData('kyoto', 1)).weather).toBeDefined();
+    expect((await weather.getForecastData('kyoto', 1)).location).toBeDefined();
 });
