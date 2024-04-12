6af3555097bbaee62afeb7dab63bec96004bcfa8
Muunatic <ichigoez0@gmail.com>
2024-04-12 07:42:27 +0700

    test: add coverage config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
-    forceExit: true,
+    collectCoverage: true,
+    coverageProvider: 'v8',
+    forceExit: false,
     verbose: true,
     testMatch: ['**/__tests__/**/*.test.cjs'],
     testPathIgnorePatterns: ['node_modules']
