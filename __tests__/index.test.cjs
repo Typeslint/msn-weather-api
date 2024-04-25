d25315b012048939b3394d6f9b188992099ddd1b
Muunatic <ichigoez0@gmail.com>
2024-04-25 11:19:52 +0700

    test: coverage function result

--- __tests__/index.test.cjs
+++ __tests__/index.test.cjs
@@ -37,21 +37,24 @@ describe('Validate MSN Weather', () => {
 
 describe('MSN Weather Data', () => {
     test('Get getCurrentData()', async () => {
-        const { temperature, humidity, location, weather, windspeed } = (await weatherjs.getCurrentData('kyoto'));
+        const { temperature, humidity, location, weather, windspeed, date, icon } = (await weatherjs.getCurrentData('kyoto'));
         expect(temperature).toBeDefined();
         expect(humidity).toBeDefined();
         expect(location).toBeDefined();
         expect(weather).toBeDefined();
         expect(windspeed).toBeDefined();
+        expect(date).toBeDefined();
+        expect(icon).toBeDefined();
     });
 
     test('Get getForecastData()', async () => {
-        const { date, day, highTemperature, location, lowTemperature, weather } = (await weatherjs.getForecastData('kyoto', 1));
+        const { date, day, highTemperature, location, lowTemperature, weather, icon } = (await weatherjs.getForecastData('kyoto', 1));
         expect(date).toBeDefined();
         expect(day).toBeDefined();
         expect(highTemperature).toBeDefined();
         expect(location).toBeDefined();
         expect(lowTemperature).toBeDefined();
         expect(weather).toBeDefined();
+        expect(icon).toBeDefined();
     });
 });
